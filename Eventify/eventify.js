4e6fcf317f64d7b2c211fdad7e4f2494a2c570eb
KennC834 <KennC834@gmail.com>
2025-04-16 22:53:08 -0400

    Push all changes

--- Eventify/eventify.js
+++ Eventify/eventify.js
@@ -1,12 +1,10 @@
-const express = require('express');
+import express from 'express';
 const app = express();
 const PORT = 3000;
 
-
-app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
-
 
 let events = [
     { name: 'Tech Expo 2025', date: '2025-05-10' },
@@ -17,16 +15,13 @@ app.get('/events', (req, res) => {
     res.render('events', { title: 'Events - Eventify', events });
 });
 
-
 app.get('/', (req, res) => {
     res.render('index', { title: 'Home - Eventify', events });
 });
 
-
 app.get('/submit', (req, res) => {
     res.render('submit', { title: 'Submit Event - Eventify' });
 });
-
 
 app.post('/submit', (req, res) => {
     const { name, date } = req.body;
@@ -34,16 +29,13 @@ app.post('/submit', (req, res) => {
     res.redirect('/');
 });
 
-
 app.get('/login', (req, res) => {
     res.render('login', { title: 'Login - Eventify' });
 });
 
-
 app.get('/register', (req, res) => {
     res.render('register', { title: 'Register - Eventify' });
 });
-
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
