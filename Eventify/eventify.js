1ad8e0017d69b676c7b8e1245c59c6bf0260075c
KennC834 <KennC834@gmail.com>
2025-05-03 15:48:51 -0400

    Started work on the register page. I then started implementing a DB into the project.

--- Eventify/eventify.js
+++ Eventify/eventify.js
@@ -1,10 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import User from './models/User.js';
 const app = express();
 const PORT = 3000;
 
+
+dotenv.config();
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.error('MongoDB connection error:', err));
 
 let events = [
     { name: 'Tech Expo 2025', date: '2025-05-10' },
@@ -36,6 +45,24 @@ app.get('/login', (req, res) => {
 app.get('/register', (req, res) => {
     res.render('register', { title: 'Register - Eventify' });
 });
+
+app.post('/register', async (req, res) => {
+    const {name, email, password} = req.body;
+    try {
+        const newUser = new User({
+            name,
+            email,
+            password,
+        });
+
+        await newUser.save();
+        res.redirect('/');
+    }catch(err) {
+        console.error(err);
+        console.log(err);
+        res.status(500).send('Internal Server Error');
+    }
+})
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
